fb1ec694e4df8269244a01d60423631a1eda8677
David Lichliter <davidlichliter1999@gmail.com>
2022-01-10 20:31:31 -0700

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<p>Hello World</p>',
+  templateUrl: './app.component.html',
+  styleUrls: [
+    './app.component.css',
+  ],
 })
 export class AppComponent {}
