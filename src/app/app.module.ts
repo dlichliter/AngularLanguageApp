f711a15f649a8943231e201bde58109c92d331cb
David Lichliter <davidlichliter1999@gmail.com>
2022-01-13 18:49:53 -0700

    Notification, score, and question components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from "@angular/platform-browser";
-import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   bootstrap: [
@@ -11,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   declarations: [
     AppComponent,
+    QuestionComponent,
+    NotificationComponent,
+    ScoreComponent,
   ],
   imports: [
     BrowserModule,
